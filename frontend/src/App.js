2c6c3d7d429f31e4ab6d62a2cee1073a1c0902ea
Kulveer Kaur <kulveerkaurpctebtech20@gmail.com>
2023-03-31 09:57:12 +0530

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>SS Industries</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
